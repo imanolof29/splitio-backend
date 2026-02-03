41ed1736a9b806cf2d7d25a072b97cbe67a37d0e
Imanol Ortiz <imanolof29@gmail.com>
2026-02-03 18:17:57 +0100

    first commit

---
+++ src/main/kotlin/com/imanolortiz/splitio/SplitioApplication.kt
@@ -0,0 +1,11 @@
+package com.imanolortiz.splitio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SplitioApplication
+
+fun main(args: Array<String>) {
+	runApplication<SplitioApplication>(*args)
+}
---
+++ src/test/kotlin/com/imanolortiz/splitio/SplitioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.imanolortiz.splitio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SplitioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
